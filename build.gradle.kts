e0f29b0a433eed9242e5059dfdd63e7df6b8639f
t.momose <hyakkun@gmail.com>
2024-08-18 18:17:33 +0900

    Setup gradle

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+val kotestVersion: String by project
+val kotlinVersion: String by project
+val liquibaseVersion: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "momosetkn"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // test
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-framework-engine-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+
+    // db-migration
+    implementation("org.liquibase:liquibase-core:$liquibaseVersion")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation(kotlin("stdlib-jdk8"))
+
+    // kotlin-script
+    implementation("org.jetbrains.kotlin:kotlin-scripting-common:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:$kotlinVersion")
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-script-util:1.8.22")
+//    implementation(kotlin("reflect"))
+
+    // reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        freeCompilerArgs.addAll(listOf("-Xcontext-receivers", "-Xjvm-default=all"))
+    }
+}
