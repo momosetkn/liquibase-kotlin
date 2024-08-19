8857cf8dc9a7cf4eecc1abc06308c79dd2dba48a
t.momose <hyakkun@gmail.com>
2024-08-19 21:57:06 +0900

    Fix databaseChangeLog function signature

--- dsl/src/main/kotlin/momosetkn/liquibase/kotlin/dsl/ChangeLogBuilderDsl.kt
+++ dsl/src/main/kotlin/momosetkn/liquibase/kotlin/dsl/ChangeLogBuilderDsl.kt
@@ -12,41 +12,41 @@ class ChangeLogBuilderDsl(
     private val resourceAccessor: ResourceAccessor,
 ) {
     fun databaseChangeLog(
-        // assigned by momosetkn.liquibase.kotlin.parser.KotlinLiquibaseChangeLogParser.updateChangeLog
-        physicalFilePath: String? = changeLog.physicalFilePath,
-        logicalFilePath: String? = null,
-        objectQuotingStrategy: String? = null,
-//        changeLogParameters: Map<String, Any>? = null,
-//        runtimeEnvironment: Map<String, Any>? = null,
-        rootChangeLog: DatabaseChangeLog? = null,
-        parentChangeLog: String? = null,
-        context: String? = null, //  same to contextFilter
-        contextFilter: String? = null,
-        includeContextFilter: String? = null,
-        includeLabels: String? = null,
-        includeIgnore: Boolean? = null,
+//        // assigned by momosetkn.liquibase.kotlin.parser.KotlinLiquibaseChangeLogParser.updateChangeLog
+//        physicalFilePath: String? = changeLog.physicalFilePath,
+//        logicalFilePath: String? = null,
+//        objectQuotingStrategy: String? = null,
+////        changeLogParameters: Map<String, Any>? = null,
+////        runtimeEnvironment: Map<String, Any>? = null,
+//        rootChangeLog: DatabaseChangeLog? = null,
+//        parentChangeLog: String? = null,
+//        context: String? = null, //  same to contextFilter
+//        contextFilter: String? = null,
+//        includeContextFilter: String? = null,
+//        includeLabels: String? = null,
+//        includeIgnore: Boolean? = null,
         block: (ChangeLogDsl).() -> Unit,
     ) {
-        changeLog.physicalFilePath = physicalFilePath
-        changeLog.logicalFilePath = logicalFilePath
-        objectQuotingStrategy?.also {
-            changeLog.objectQuotingStrategy = ObjectQuotingStrategy.valueOf(it)
-        }
-        changeLog.rootChangeLog = rootChangeLog
-        parentChangeLog?.also {
-            changeLog.parentChangeLog = DatabaseChangeLog(parentChangeLog)
-        }
-        val contextFilterOrContext = contextFilter ?: context
-        contextFilterOrContext?.also {
-            changeLog.contextFilter = ContextExpression(contextFilterOrContext)
-        }
-        includeContextFilter?.also {
-            changeLog.includeContextFilter = ContextExpression(it)
-        }
-        includeLabels?.also {
-            changeLog.includeLabels = Labels(it)
-        }
-        changeLog.isIncludeIgnore = includeIgnore ?: false
+//        changeLog.physicalFilePath = physicalFilePath
+//        changeLog.logicalFilePath = logicalFilePath
+//        objectQuotingStrategy?.also {
+//            changeLog.objectQuotingStrategy = ObjectQuotingStrategy.valueOf(it)
+//        }
+//        changeLog.rootChangeLog = rootChangeLog
+//        parentChangeLog?.also {
+//            changeLog.parentChangeLog = DatabaseChangeLog(parentChangeLog)
+//        }
+//        val contextFilterOrContext = contextFilter ?: context
+//        contextFilterOrContext?.also {
+//            changeLog.contextFilter = ContextExpression(contextFilterOrContext)
+//        }
+//        includeContextFilter?.also {
+//            changeLog.includeContextFilter = ContextExpression(it)
+//        }
+//        includeLabels?.also {
+//            changeLog.includeLabels = Labels(it)
+//        }
+//        changeLog.isIncludeIgnore = includeIgnore ?: false
         val dsl =
             ChangeLogDsl(
                 changeLog = changeLog,
