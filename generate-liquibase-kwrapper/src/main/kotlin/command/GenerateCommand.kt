d01c4df1207b8bdd680df5e9f23eb6d0b71a9bd5
t.momose <hyakkun@gmail.com>
2024-09-09 00:23:56 +0900

    Fix GenerateCommand

--- generate-liquibase-kwrapper/src/main/kotlin/command/GenerateCommand.kt
+++ generate-liquibase-kwrapper/src/main/kotlin/command/GenerateCommand.kt
@@ -1,6 +1,7 @@
 package momosetkn.command
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 fun generateCommand(uri: String): String {
     val doc =
@@ -11,30 +12,11 @@ fun generateCommand(uri: String): String {
     println(doc.title())
     val command = doc.select("h1")[0].text()
     val methodName = command.toCamelCase()
-    val newsHeadlines = doc.select("#cli > table")
-    val availableAttributes = newsHeadlines[0]
-//    val databaseSupport = newsHeadlines[1]
-    val trs = availableAttributes.select("tbody > tr")
-    val items =
-        trs.map { tr ->
-            val td = tr.select("td")
-            val td0 = td[0].text()
-            val (rawName1, rawType) = td0.split("=")
-            val name = rawName1.removePrefix("--").toCamelCase()
-            val type = mapOf(
-                "<string>" to "String",
-                "<true|false>" to "Boolean",
-                "<int>" to "Int",
-            )[rawType]
-            checkNotNull(type)
-            val requirement = td[2].text()
-            Item(
-                liquibaseName = rawName1,
-                name = name,
-                type = type,
-                required = requirement == "Required",
-            )
-        }
+    val tables = doc.select("#cli > table")
+    val items = tables.flatMap { table ->
+        val trs = table.select("tbody > tr")
+        trs.map(::item)
+    }.mapNotNull { it }
     val formatedArgs =
         items.joinToString("\n") {
             val typeString =
@@ -68,6 +50,28 @@ fun generateCommand(uri: String): String {
             }
         """.trimIndent()
 }
+
+private fun item(tr: Element): Item? {
+    val td = tr.select("td")
+    if (td.size == 0) return null
+    val td0 = td[0].text()
+    val (rawName1, rawType) = td0.split("=")
+    val name = rawName1.removePrefix("--").toCamelCase()
+    val type = mapOf(
+        "<string>" to "String",
+        "<true|false>" to "Boolean",
+        "<int>" to "Int",
+    )[rawType]
+    checkNotNull(type)
+    val requirement = td[2].text()
+    return Item(
+        liquibaseName = rawName1,
+        name = name,
+        type = type,
+        required = requirement == "Required",
+    )
+}
+
 val delimiter = Regex("[ -]")
 fun String.toCamelCase(): String {
     return this.split(delimiter)
