298c8aec3230f4dc5f1cd5c61598e39c861c7467
t.momose <hyakkun@gmail.com>
2024-09-11 21:50:04 +0900

    Refactoring CustomChangeWrapper.setCustomChange fun

--- dsl/src/main/kotlin/liquibase/change/custom/CustomChangeWrapperExtension.kt
+++ dsl/src/main/kotlin/liquibase/change/custom/CustomChangeWrapperExtension.kt
@@ -1,5 +1,7 @@
 package liquibase.change.custom
 
-fun CustomChangeWrapper.setCustomChange(customChange: CustomChange) {
+internal fun CustomChangeWrapper.setCustomChange(customChange: CustomChange) {
+    // this.customChange is package private.
+    // it is necessary to perform this configuration process within the same package.
     this.customChange = customChange
 }
