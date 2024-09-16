5eb11467084983c4944b4dd1190ed81833cb091a
t.momose <hyakkun@gmail.com>
2024-09-17 00:22:16 +0900

    Add comment to KotlinScriptImports

--- script-parser/src/main/kotlin/momosetkn/liquibase/kotlin/parser/KotlinScriptImports.kt
+++ script-parser/src/main/kotlin/momosetkn/liquibase/kotlin/parser/KotlinScriptImports.kt
@@ -1,5 +1,9 @@
 package momosetkn.liquibase.kotlin.parser
 
+/**
+ * import on script-parser.
+ * when exists `script-parser` module, It will be loaded with ServiceLoader in `script-parser`.
+ */
 interface KotlinScriptImports {
     fun imports(): List<String>
 }
