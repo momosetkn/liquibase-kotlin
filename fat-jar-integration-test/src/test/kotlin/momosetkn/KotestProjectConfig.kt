16e6d028aa35514915a8c1413c4aa06c5ef9710d
t.momose <hyakkun@gmail.com>
2024-09-23 17:48:40 +0900

    try

--- fat-jar-integration-test/src/test/kotlin/momosetkn/KotestProjectConfig.kt
+++ fat-jar-integration-test/src/test/kotlin/momosetkn/KotestProjectConfig.kt
@@ -5,7 +5,7 @@ import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 class KotestProjectConfig : AbstractProjectConfig() {
-    override val projectTimeout = 5.minutes
+    override val projectTimeout = 30.minutes
     override val timeout = 30.seconds
-    override val invocationTimeout = 10.seconds.inWholeMilliseconds
+    override val invocationTimeout = 1_000L
 }
