b8ccec4ad801ed03ce99046a61595d1edc4ab6f3
t.momose <hyakkun@gmail.com>
2024-09-23 17:01:04 +0900

    try

--- fat-jar-integration-test/build.gradle.kts
+++ fat-jar-integration-test/build.gradle.kts
@@ -35,8 +35,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
     systemProperty("kotest.framework.classpath.scanning.config.disable", "true")
-    systemProperty("kotest.framework.timeout", 10_000)
-    systemProperty("kotest.framework.invocation.timeout", 5_000)
+    systemProperty("kotest.framework.timeout", 30_000)
 }
 
 tasks {
