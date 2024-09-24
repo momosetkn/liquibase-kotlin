9857dceb6e8e4f96aa8340220e2c52eb21cfbe42
t.momose <hyakkun@gmail.com>
2024-09-24 23:37:11 +0900

    Fix LiquibaseCommandExecutor

--- client/src/main/kotlin/momosetkn/liquibase/client/LiquibaseCommandExecutor.kt
+++ client/src/main/kotlin/momosetkn/liquibase/client/LiquibaseCommandExecutor.kt
@@ -1,37 +1,9 @@
 package momosetkn.liquibase.client
 
 import liquibase.command.CommandScope
-import liquibase.integration.commandline.LiquibaseCommandLine
-import java.util.concurrent.Executors
 
 object LiquibaseCommandExecutor {
-    /**
-     * Limit the execution of Liquibase to a single thread.
-     * One reason is to prevent interference between migrators,
-     * but there are also the following issues, so it will be executed on a fixed thread.
-     * ```
-     * Error below when executing Liquibase in parallel (executing in a different thread from the first execution).
-     *
-     * Unexpected argument(s):
-     * changelog-file=db/..., url=jdbc:..., driver=com..., username=root, password=test
-     *
-     * For detailed help, try 'liquibase help' or 'liquibase <command-name> help'
-     */
-    private val executor = Executors.newSingleThreadExecutor()
-    private val cli = LiquibaseCommandLine()
     private val log = org.slf4j.LoggerFactory.getLogger(this::class.java)
-
-    init {
-        Runtime.getRuntime().addShutdownHook(
-            Thread {
-                shutdown()
-            },
-        )
-    }
-
-    fun shutdown() {
-        executor.shutdown()
-    }
 
     fun executeCommand(
         command: List<String>,
@@ -43,35 +15,24 @@ object LiquibaseCommandExecutor {
             val (key, value) = arg
             commandScope.addArgumentValue(key, value)
         }
+        logArgs(command, args)
         commandScope.execute()
     }
 
-    fun executeLiquibaseCommandLine(args: List<String>) {
-        val typedArgs = args.toTypedArray()
-        logArgs(typedArgs)
-        // In the following case, it will terminate the process using System.exit.
-        // liquibase.integration.commandline.LiquibaseCommandLine.main
-        val future =
-            executor.submit<Int> {
-                cli.execute(typedArgs)
-            }
-        val returnCode = future.get()
-        check(returnCode == 0) {
-            "liquibase command failed, return code: $returnCode"
-        }
-    }
-
-    private fun logArgs(args: Array<String>) {
+    private fun logArgs(
+        command: List<String>,
+        args: List<Pair<String, String>>,
+    ) {
         val maskedArgs = passwordMasking(args)
-        log.info("execute liquibase. params: ${maskedArgs.joinToString(" ")}")
+        log.info("execute liquibase. command: {}, params: {},", command, maskedArgs.joinToString(" "))
     }
 
-    private fun passwordMasking(args: Array<String>): List<String> =
-        args.map { arg ->
-            if ("password" in arg.substringBefore("=")) {
-                arg.replaceAfter("=", "***masked***")
+    private fun passwordMasking(args: List<Pair<String, String>>): List<String> =
+        args.map { (key, value) ->
+            if (key.contains("password", ignoreCase = true)) {
+                key to "***masked***"
             } else {
-                arg
+                key to value
             }
-        }
+        }.map { (key, value) -> "$key=$value" }
 }
