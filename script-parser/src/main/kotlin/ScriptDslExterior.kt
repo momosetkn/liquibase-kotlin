8b5c7dfa2346ac0cee578689b61646a5ab2168a3
t.momose <hyakkun@gmail.com>
2024-10-13 05:10:38 +0900

    Fix the script-parser for concurrency.

--- script-parser/src/main/kotlin/ScriptDslExterior.kt
+++ script-parser/src/main/kotlin/ScriptDslExterior.kt
@@ -1,13 +1,11 @@
 import momosetkn.liquibase.kotlin.dsl.ChangeLogDsl
-import momosetkn.liquibase.kotlin.parser.ChangeLogDslBlocks
 
 /**
  * Kotlin script exterior.
  * For convenience, root scope.
+ * It exists only for the purpose of autocompletion in the IDE. therefore method-body is empty.
  * same to [momosetkn.liquibase.kotlin.dsl.ChangeLogBuilderDsl.databaseChangeLog]
  */
 fun databaseChangeLog(
     block: (ChangeLogDsl).() -> Unit,
-) {
-    ChangeLogDslBlocks.items.add(block)
-}
+) {}
