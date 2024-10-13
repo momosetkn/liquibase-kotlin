9da3efbefada902a64b8b9179df95de67c7ce813
t.momose <hyakkun@gmail.com>
2024-10-13 20:39:34 +0900

    Improving client build.gradle.kts

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,12 +1,32 @@
-// liquibase-kotlin-client is tested only this version
-val latestLiquibaseVersion = "4.29.2"
+import java.util.Properties
+
 val kotestVersion = rootProject.properties["kotestVersion"] as String
 val slf4jVersion = rootProject.properties["slf4jVersion"] as String
 val kotlinVersion = rootProject.properties["kotlinVersion"] as String
+// liquibase-kotlin-client is tested snapshot or latest-stable version
+val liquibaseVersion = getSnapshotOrDefaultVersion("liquibaseVersion")
+
+fun loadDefaultProperty(propertyName: String): String {
+    val gradleProps = Properties()
+    val gradlePropertiesFile = file(rootProject.path).toPath().resolveSibling("gradle.properties").toFile()
+    gradlePropertiesFile.inputStream().use { gradleProps.load(it) }
+    return gradleProps.getProperty(propertyName)
+}
+
+fun getSnapshotOrDefaultVersion(propertyName: String): String {
+    val version = rootProject.properties[propertyName] as String
+    return if (version.any { it.isLetter() }) {
+        // snapshot
+        version
+    } else {
+        // latest-stable
+        loadDefaultProperty(propertyName)
+    }
+}
 
 dependencies {
     // liquibase
-    implementation("org.liquibase:liquibase-core:$latestLiquibaseVersion")
+    implementation("org.liquibase:liquibase-core:$liquibaseVersion")
     // log
     api("org.slf4j:slf4j-api:$slf4jVersion")
 
