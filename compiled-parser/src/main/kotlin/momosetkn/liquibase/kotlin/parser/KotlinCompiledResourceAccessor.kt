9f19470fb57613dd9d89fd82f9e3d8e225ed117c
t.momose <hyakkun@gmail.com>
2024-10-14 20:42:59 +0900

    Refactoring KotlinCompiledResourceAccessor

--- compiled-parser/src/main/kotlin/momosetkn/liquibase/kotlin/parser/KotlinCompiledResourceAccessor.kt
+++ compiled-parser/src/main/kotlin/momosetkn/liquibase/kotlin/parser/KotlinCompiledResourceAccessor.kt
@@ -9,37 +9,38 @@ import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 
 /**
+ * This ResourceAccessor for can specifying packageName with include/includeAll change.
  * This class implements the `ResourceAccessor` interface to provide access to compiled Kotlin classes.
  * This class is find the class or package with delegate to `ClassLoaderResourceAccessor`.
  */
 class KotlinCompiledResourceAccessor(
     private val classLoaderResourceAccessor: ClassLoaderResourceAccessor = ClassLoaderResourceAccessor(),
 ) : ResourceAccessor by classLoaderResourceAccessor {
-    override fun search(path: String, recursive: Boolean): MutableList<Resource> {
-        return classLoaderResourceAccessor.search(toJavaIdentifierName(path), recursive)
+    override fun search(path: String, recursive: Boolean): List<Resource> {
+        return classLoaderResourceAccessor.search(containPackageNameToClasspathName(path), recursive)
     }
 
-    override fun getAll(path: String): MutableList<Resource> {
-        return classLoaderResourceAccessor.getAll(toJavaIdentifierName(path))
+    override fun getAll(path: String): List<Resource> {
+        return classLoaderResourceAccessor.getAll(containPackageNameToClasspathName(path))
     }
 
     /**
-     * To transform java indetification name.
+     * Transform contain package name filepath to name on classpath.
      *
      * example
      * aaa/bbb/com.example.hoge/hogehoge.class -> aaa/bbb/com/example/hoge/hogehoge.class
      * aaa/bbb/com.example.hoge/ -> aaa/bbb/com/example/hoge/
      */
-    private fun toJavaIdentifierName(root: String): String {
-        val replacedRoot = root
+    private fun containPackageNameToClasspathName(containPackageName: String): String {
+        val replaced = containPackageName
             .replace("file:", "")
             .replace("\\", "/")
         val decodedRoot = try {
-            URLDecoder.decode(replacedRoot, StandardCharsets.UTF_8.name())
+            URLDecoder.decode(replaced, StandardCharsets.UTF_8.name())
         } catch (e: UnsupportedEncodingException) {
             Scope.getCurrentScope().getLog(javaClass)
-                .fine("Failed to decode path $root; continuing without decoding.", e)
-            replacedRoot
+                .fine("Failed to decode path $containPackageName; continuing without decoding.", e)
+            replaced
         }
         val splited = decodedRoot.split("/")
         val init = splited.dropLast(2)
