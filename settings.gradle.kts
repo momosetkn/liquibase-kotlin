f7f3fe56096cdbc7dafa01c5d23cdac67cc3f870
t.momose <hyakkun@gmail.com>
2024-11-16 18:46:26 +0900

    Refactoring build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,14 @@
 pluginManagement {
     val kotlinVersion: String by settings
+    val kspVersion: String by settings
+    val komapperVersion: String by settings
     repositories {
         gradlePluginPortal()
     }
     plugins {
         kotlin("jvm") version kotlinVersion
+        id("com.google.devtools.ksp") version kspVersion
+        id("org.komapper.gradle") version komapperVersion
     }
 }
 
