4f1ee154ab12ffc5ed21ed803e61ef306dc86574
t.momose <hyakkun@gmail.com>
2025-09-11 21:42:18 +0900

    Fix regex in `maskChangeSetParams` to properly handle multi-digit change set IDs

--- test-utils/src/main/kotlin/momosetkn/utils/MaskChangeSetParams.kt
+++ test-utils/src/main/kotlin/momosetkn/utils/MaskChangeSetParams.kt
@@ -1,6 +1,6 @@
 package momosetkn.utils
 
-private val changeSetRegex = Regex("""changeSet\(author = "(.+)", id = "(\d+)-(\d)"\) \{""")
+private val changeSetRegex = Regex("""changeSet\(author = "(.+)", id = "(\d+)-(\d+)"\) \{""")
 
 fun String.maskChangeSetParams() =
     this.replace(changeSetRegex, "changeSet(author = \"**********\", id = \"*************-\$3\") {")
